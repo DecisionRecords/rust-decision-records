16b0838694eef0b85e0a85913546fe8f0641c314
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2021-10-27 14:27:12 +0100

    Fixed formatting

--- src/init.rs
+++ src/init.rs
@@ -8,13 +8,19 @@ pub fn init(
     template_directory: PathBuf,
     adr_format: bool,
     default_proposed: bool,
-    force: bool
+    force: bool,
 ) {
-    println!("doc_path: {}", doc_path.into_os_string().into_string().unwrap());
+    println!(
+        "doc_path: {}",
+        doc_path.into_os_string().into_string().unwrap()
+    );
     println!("template_file: {}", template_file);
     println!("format: {}", format);
     println!("language: {}", language);
-    println!("template_directory: {}", template_directory.into_os_string().into_string().unwrap());
+    println!(
+        "template_directory: {}",
+        template_directory.into_os_string().into_string().unwrap()
+    );
     println!("adr_format: {}", adr_format);
     println!("default_proposed: {}", default_proposed);
     println!("force: {}", force);
