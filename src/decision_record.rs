b6a97ce690ba69e8b7dd220a737fa569620b5421
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2021-12-03 22:42:29 +0000

    Define file name with "slug" and directory parsing

--- src/decision_record.rs
+++ src/decision_record.rs
@@ -1,4 +1,9 @@
+extern crate slug;
+use slug::slugify;
+
 use crate::config;
+use regex::Regex;
+use std::fs;
 
 pub fn new_record(
     title: String,
@@ -23,6 +28,32 @@ pub fn new_record(
     println!("config.template_file: {}", config.template_file);
     println!("config.template_format: {}", config.template_format);
     println!("config.template_string: {}", config.template_string);
+    for (key, value) in config.template_placeholders {
+        println!("config.template_placeholders[{}]: {}", key, value);
+    }
+
+    let filename_structure = Regex::new(r"^(\d{4})(.*)$").unwrap();
+    let mut max_file_prefix: i32 = 0;
+
+    let paths = fs::read_dir(config.record_path);
+    if paths.is_ok() {
+        for path in paths.unwrap() {
+            let str_path = path.unwrap().path().display().to_string();
+            if filename_structure.is_match(&str_path) {
+                let file_number: i32 = filename_structure.replace(&str_path, "$1").parse().unwrap();
+                if max_file_prefix < file_number {
+                    max_file_prefix = file_number;
+                }
+            }
+        }
+    }
+    max_file_prefix = max_file_prefix + 1;
+    let mut filename: String = format!("{:0>4}", max_file_prefix);
+    filename.push_str("-");
+    filename.push_str(&String::from(slugify(title)));
+    filename.push_str(".");
+    filename.push_str(&config.template_format);
+    println!("Create file: {}", filename);
 }
 
 pub fn approve(records: String) {
